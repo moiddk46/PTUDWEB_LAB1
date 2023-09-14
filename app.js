8df25ed8f83000840bc94fb7d467d5cab8c83715
moiddk46 <moiddk46@gmail.com>
2023-09-14 11:14:20 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const req = require("express/lib/request");
 const res = require("express/lib/response");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(express.json());
 app.get("/", (req,res) =>{
     res.json({message: "Welcome to contact book application."});
 });
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
